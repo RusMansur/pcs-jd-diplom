8521c4f89b1e972726fc424407d00485f89fd1b5
RusMansur <rusdolg@gmail.com>
2022-05-01 16:12:18 +0300

    Исправлен Main и Client

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,45 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-	public static Scanner input = new Scanner(System.in);
 
 	public static void main(String[] args) throws IOException {
 		String host = "localhost";
 		int port = 8989;
 
 		Socket clientSocket = new Socket(host, port);
-		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-		BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+		DataInputStream in = new DataInputStream(clientSocket.getInputStream());
 
-		String response = in.readLine();
-		System.out.println(response);
+		out.writeUTF("развитие");
 
-		boolean connectEstablished = true;
-		while (connectEstablished) {
-			String request = input.nextLine();
-			if (request.equals("#")) {
-				out.println(request);
-				System.out.println(in.readLine());
-				connectEstablished = false;
+		boolean readAnswer = true;
+		while (readAnswer) {
+			String response = in.readUTF();
+			if (response.equals("end")) {
+				readAnswer = false;
 				continue;
 			}
-			out.println(request);
-			boolean readAnswer = true;
-			while (readAnswer) {
-				 response = in.readLine();
-				if (response.equals("end")) {
-					System.out.println("-Конец поиска-\nВведите ещё одно слово или '#', чтобы завершить");
-					readAnswer = false;
-					continue;
-				}
-				System.out.println(response);
-			}
+			System.out.println(response);
 		}
-		clientSocket.close();
+
 	}
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,7 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
@@ -14,37 +11,26 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		int port = 8989;
-		System.out.println("Сервер запущен...");
 		while (true) {
 			ServerSocket serverSocket = new ServerSocket(port);
 			Socket connection = serverSocket.accept();
 
-			PrintWriter out = new PrintWriter(connection.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+			DataInputStream in = new DataInputStream(connection.getInputStream());
 
-			out.println("Введите слово для поиска по документам:");
-			boolean listenPort = true;
-			while (listenPort) {
-				final String word = in.readLine();
-				if (word != null && word.equals("#")) {
-					out.println("Всего доброго!");
-					listenPort = false;
-					continue;
-				}
-				BooleanSearchEngine engine = new BooleanSearchEngine(new File(PATH));
-				List<PageEntry> pageEntryList = engine.search(word);
-				if (!pageEntryList.isEmpty()) {
-					for (PageEntry pageEntry : pageEntryList) {
-						GsonBuilder gsonBuilder = new GsonBuilder();
-						Gson gson = gsonBuilder.setPrettyPrinting().create();
-						out.println(gson.toJson(pageEntry));
-					}
-				} else {
-					out.println("Проверены все документы. Такого слова в них нет.");
-				}
-				out.println("end");
+			String word = in.readUTF();
+			BooleanSearchEngine engine = new BooleanSearchEngine(new File(PATH));
+			List<PageEntry> pageEntryList = engine.search(word);
+			for (PageEntry pageEntry : pageEntryList) {
+				GsonBuilder gsonBuilder = new GsonBuilder();
+				Gson gson = gsonBuilder.setPrettyPrinting().create();
+				out.writeUTF(gson.toJson(pageEntry));
 			}
-
+			out.writeUTF("end");
+			out.flush();
+			in.close();
+			out.close();
+			connection.close();
 			serverSocket.close();
 		}
 	}
